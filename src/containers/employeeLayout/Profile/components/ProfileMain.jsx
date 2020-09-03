7e0661fbfce54bf8c69404d564e3fb3418f404eb
naveed-rana <rana.naveed812@gmail.com>
2020-09-03 13:20:16 +0500

    added change password and set profile fields editable for both employee and employeer

--- src/containers/employeeLayout/Profile/components/ProfileMain.jsx
+++ src/containers/employeeLayout/Profile/components/ProfileMain.jsx
@@ -3,13 +3,14 @@ import { Card, CardBody, Col, Button, Row } from "reactstrap";
 import MessageTextOutlineIcon from "mdi-react/MessageTextOutlineIcon";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Model from "./updateModel";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { startRessetPassword } from "../../../../redux/actions/profileAction";
+import ChangePassword from "./changePassword";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPen} from '@fortawesome/free-solid-svg-icons'
-
-
-library.add(faPen)
+library.add(faPen);
 
 class ProfileMain extends React.Component {
   render() {
@@ -20,25 +21,80 @@ class ProfileMain extends React.Component {
           <CardBody>
             <div className="profile__information">
               <div style={{ width: "100%" }}>
-                <p className="profile__activity-name">Name: </p>
-                <p className="profile__name">{user.name}</p>
+                <p className="profile__activity-name">First Name: </p>
+                <Row>
+                  <Col md={6} lg={6} xl={6}>
+                    <p className="profile__contact">{user.firstName}</p>
+                  </Col>
+                  <Col md={6} lg={6} xl={6}>
+                    <Model
+                      title="First Name"
+                      type="text"
+                      value={user.firstName}
+                      newProps="firstName"
+                      docid={user.docid}
+                    />
+                  </Col>
+                </Row>
+                <p className="profile__activity-name">Last Name: </p>
+                <Row>
+                  <Col md={6} lg={6} xl={6}>
+                    <p className="profile__contact">{user.lastName}</p>
+                  </Col>
+                  <Col md={6} lg={6} xl={6}>
+                    <Model
+                      title="Last Name"
+                      type="text"
+                      value={user.lastName}
+                      newProps="lastName"
+                      docid={user.docid}
+                    />
+                  </Col>
+                </Row>
                 <p className="profile__activity-name">Email: </p>
-                <p className="profile__contact">{user.email}</p>
+                <Row>
+                  <Col md={6} lg={6} xl={6}>
+                    <p className="profile__contact">{user.email}</p>
+                  </Col>
+                </Row>
                 <p className="profile__activity-name">Cell: </p>
-                <p className="profile__contact">{user.cell}</p>
+                <Row>
+                  <Col md={6} lg={6} xl={6}>
+                    <p className="profile__contact">{user.cell}</p>
+                  </Col>
+                  <Col md={6} lg={6} xl={6}>
+                    <Model
+                      title="Cell"
+                      type="number"
+                      value={user.cell}
+                      newProps="cell"
+                      docid={user.docid}
+                    />
+                  </Col>
+                </Row>
                 <p className="profile__activity-name">Address: </p>
                 <Row>
                   <Col md={6} lg={6} xl={6}>
-                    <p>
-                      {
-                        user.address
-                      }
-                    </p>
+                    <p>{user.address}</p>
                   </Col>
                   <Col md={6} lg={6} xl={6}>
-                    <Link to="/home/employee/changeaddress" style={{ textDecoration: "none" }}>
+                    <Link
+                      to="/home/employee/changeaddress"
+                      style={{ textDecoration: "none" }}
+                    >
                       <FontAwesomeIcon icon="pen" />
                     </Link>
+                  </Col>
+                </Row>
+                <p className="profile__activity-name">Change Password: </p>
+                <Row>
+                  <Col md={6} lg={6} xl={6}>
+                    ******
+                  </Col>
+                  <Col md={6} lg={6} xl={6}>
+                    <ChangePassword
+                      changePassAction={this.props.startRessetPassword}
+                    />
                   </Col>
                 </Row>
               </div>
@@ -63,15 +119,15 @@ class ProfileMain extends React.Component {
     );
   }
 }
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   // user: state.userReducer.user,
   user: state.employeeUserReducer.currentEmp,
   // employees: state.employerReducer.employees,
   tasks: state.employeeTaskReducer.AllTask,
-  ownTasks: state.employeeTaskReducer.empOwnTask
+  ownTasks: state.employeeTaskReducer.empOwnTask,
 });
 
 export default connect(
   mapStateToProps,
-  null
+  { startRessetPassword }
 )(ProfileMain);
